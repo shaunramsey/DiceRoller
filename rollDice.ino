172c77a2208b9805c21b360110c89188059e23f9
csaul2 <csaul3@washcoll.edu>
2017-05-22 15:32:31 -0400

    Cut out unneeded stuff

--- rollDice.ino
+++ rollDice.ino
@@ -6,6 +6,9 @@
 /* v0.3 - added a function that tests each segment in order A-G,dp */
 /* v0.2 - added comments and rearranged some code */
 
+const int clockPin = 11;
+const int latchPin = 12;
+const int dataPin = 13;
 
 //a map of the digits
 static const byte digitMap[] = {
@@ -51,20 +54,19 @@ static const byte charMap[] = {
  B01011011, // 90  'Z'  Same as '2'
 };
 
-//map your a-g,dp to pins on the arduino...probably could do this in
-//a more orderly fashion but this array allows us to mix things up
-                            //a, b, c, d, e, f, g, dp
-const int alphaToIndex[] =  {3,  2, 8, 7, 6, 4, 5, 9};//{ 7,6,4,2,1,9,10,5};
-
-//commented out because this is for a different arduino fritzing
-//const int segmentOrder[] = {1, 0, 6, 5, 4, 2, 3, 7};
-
 //This containts the segment order for GIS day 
 const int segmentOrder[] = {3, 4, 0, 2, 1, 6, 5, 7};
 
-const int clockPin = 11;
-const int latchPin = 12;
-const int dataPin = 13;
+//will adjust the digit bytes to match the arduino setup 
+byte permute(byte d) {
+  byte b = 0;
+  for (int i=0; i<8; i++){
+    //Does exactly what the Bitwrite line does but faster using bitshifts 
+    b = b  | ( ( (d & ( 1 << i ) ) >> i ) << segmentOrder[i] );
+    //bitWrite(b, segmentOrder[i], bitRead(d, i));
+  }
+  return b;
+}
 
 //mixed case and some are missing
 void displayCharacter(char c) {
@@ -72,27 +74,13 @@ void displayCharacter(char c) {
    int index = c - 'a';
    //uses XOR and the -1 value to invert the byte. 
    //XOR making the value true when they differ
-   byte d = charMap[index] ^ -1;
+   byte d = permute(charMap[index]) ^ -1;
    digitalWrite(latchPin, LOW);
    shiftOut(dataPin, clockPin, LSBFIRST, d);
    digitalWrite(latchPin, HIGH);
 }
 
-
-//display this particular digit
 void displayDigit(int index, int period) {
-   //uses XOR and the -1 value to invert the byte. 
-   //XOR making the value true when they differ
-   byte d = digitMap[index] ^ -1;
-   if (period == HIGH){
-    d = d & 127; 
-   }
-   digitalWrite(latchPin, LOW);
-   shiftOut(dataPin, clockPin, LSBFIRST, d);
-   digitalWrite(latchPin, HIGH);
-}
-
-void displayDigit2(int index, int period) {
    //uses XOR and the -1 value to invert the byte. 
    //XOR making the value true when they differ
    byte d = permute(digitMap[index]) ^ -1;
@@ -113,35 +101,11 @@ void turnOffPause(int pause_length) {
   delay(pause_length);
 }
 
-
-//will adjust the digit bytes to match the arduino setup 
-byte permute(byte d) {
-  byte b = 0;
-  for (int i=0; i<8; i++){
-    //Does exactly what the Bitwrite line does but faster using bitshifts 
-    b = b  | ( ( (d & ( 1 << i ) ) >> i ) << segmentOrder[i] );
-    //bitWrite(b, segmentOrder[i], bitRead(d, i));
-  }
-  return b;
-}
-
 //Segment tests the arduino where segments dont match the digit bytes 
-void segmentTest2() {
-  for(int i = 0; i < 8; i++) {
-    byte d = B11111111;
-    bitWrite(d, segmentOrder[i], 0);
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, d);
-    digitalWrite(latchPin, HIGH);
-    delay(200);
-  }
-}
-
-//test all the segments
 void segmentTest() {
   for(int i = 0; i < 8; i++) {
     byte d = B11111111;
-    bitWrite(d, i, 0);
+    bitWrite(d, segmentOrder[i], 0);
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, LSBFIRST, d);
     digitalWrite(latchPin, HIGH);
@@ -157,18 +121,19 @@ void setup() {
   pinMode(dataPin, OUTPUT);
 
   //do a segmentTest!
-  segmentTest2();
+  segmentTest();
   randomSeed(analogRead(0)); //neat way to get a seed
   int ran = random(1,7);  
   int dur = random(1,5);  //makes dice roll feel good
   for(int i = 0; i < ran + 6*dur; i++){
     int k = i % 6 + 1;
-    displayDigit2(k, LOW);
+    displayDigit(k, LOW);
     delay(15*i + 20);
   }
-  displayDigit2(ran, HIGH);  //shows the rolled value
+  displayDigit(ran, HIGH);  //shows the rolled value
 }
 
 // loop forever
 void loop() {
+  //No loop for you
 }
